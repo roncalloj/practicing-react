9dab5ad376c0975e82bfc02d83e92f1425658878
roncalloj <roncalloj@outlook.com>
2024-03-02 23:24:11 -0500

    adding CSS and some hooks

--- projects/05-movie-browser/src/App.jsx
+++ projects/05-movie-browser/src/App.jsx
@@ -1,14 +1,40 @@
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import { MoviesResults } from './components/Movies.jsx';
 import { useMovies } from './hooks/useMovies.js';
 
+function useQuery() {
+	const [query, setQuery] = useState('');
+	const [error, setError] = useState(null);
+	const firstInput = useRef(true);
+
+	useEffect(() => {
+		if (firstInput.current) {
+			firstInput.current = query === '';
+			console.log(firstInput.current);
+			return;
+		}
+		if (query === '') {
+			setError('No movie title to browse');
+			return;
+		}
+		setError(null);
+	}, [query]);
+
+	return { query, setQuery, error };
+}
+
 function App() {
 	const { movies } = useMovies();
+	const { query, setQuery, error } = useQuery();
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
-		const data = new window.FormData(event.target);
-		const query = data.get('query');
+		console.log({ query });
+	};
+
+	const handleChange = (event) => {
+		setQuery(event.target.value);
 	};
 
 	return (
@@ -16,9 +42,15 @@ function App() {
 			<h1>Movie browser</h1>
 			<header>
 				<form onSubmit={handleSubmit}>
-					<input name="query" placeholder="Amelie, Blade runner 2043, Rocky..." />
+					<input
+						onChange={handleChange}
+						value={query}
+						name="query"
+						placeholder="Amelie, Blade runner 2043, Rocky..."
+					/>
 					<button type="submit">Search</button>
 				</form>
+				{error && <p className="error">{error}</p>}
 			</header>
 
 			<main>
